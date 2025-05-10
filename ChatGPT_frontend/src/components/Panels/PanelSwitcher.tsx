37662e97461ebc4f3cfda2bd3b0d2ba10a82e3b2
s2yao <s2yao@uwaterloo.ca>
2025-05-10 19:01:21 -0400

    frontend

--- ChatGPT_frontend/src/components/Panels/PanelSwitcher.tsx
+++ ChatGPT_frontend/src/components/Panels/PanelSwitcher.tsx
@@ -25,7 +25,7 @@ export default function PanelSwitcher({
   
     if (visible) {
       setShouldRender(true);
-      timeout = setTimeout(() => setAnimate(true), 20);
+      timeout = setTimeout(() => setAnimate(true), 10);
     } else {
       setAnimate(false);
       timeout = setTimeout(() => {
